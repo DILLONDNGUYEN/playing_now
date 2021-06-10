e2e01273b7aa1f35165eeb15319d2c1c63f11780
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-06-10 09:02:45 -0700

    add semi colons

---
+++ frontend/src.js
@@ -0,0 +1,80 @@
+const musicContainer = document.getElementById('music-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+
+//Song Titles
+
+const songs = ['Electricity', 'Be Right There', 'All I Want vs. Moments vs. Sound of Walking Away (Nick Gunner Mashup)'];
+
+//Track Songs
+
+let songIndex = 0;
+
+// Load song into DOM
+
+loadSong(songs[songIndex]);
+
+//update song
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+function playSong(){
+musicContainer.classList.add('play');
+playBtn.querySelector('i.fas').classList.remove('fa-play');
+playBtn.querySelector('i.fas').classList.add('fa-play');
+audio.play();
+}
+
+function pauseSong(){
+musicContainer.classList.remove('play');
+playBtn.querySelector('i.fas').classList.add('fa-play');
+playBtn.querySelector('i.fas').classList.remove('fa-play');
+audio.pause();
+}
+
+function prevSong(){
+  songIndex--;
+  if(songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(song[songIndex]);
+  playSong();
+
+}
+
+function nextSong(){
+  songIndex++;
+  if(songIndex)
+}
+
+
+
+
+
+
+//Event Listens
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if(isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+})
+
+//Change Event
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
