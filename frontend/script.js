45d39a75c2e1de39abe16e70b7b7849dfad32d2c
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-06-09 21:23:39 -0700

    add songs, function loadsong, and evenlisteners

--- frontend/script.js
+++ frontend/script.js
@@ -7,3 +7,35 @@ const nextbtn = document.querySelector('#next')
 const audio = document.querySelector('#audio')
 const progress = document.querySelector('.progress')
 const progressContainer = document.querySelector('.progress-container')
+
+//Song Titles
+
+const songs = ['Electricity', 'Be Right There', 'All I Want vs. Moments vs. Sound of Walking Away (Nick Gunner Mashup)']
+
+//Track Songs
+
+let songIndex = 0
+
+// Load song into DOM
+
+loadSong(songs[songIndex])
+
+//update song
+
+function loadSong(song) {
+  title.innerText = song
+  audio.src = `music/${song}.mp3`
+  cover.src = `images/${song}.jpg`
+}
+
+//Event Listens
+
+playbtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play')
+
+  if(isPlaying) {
+    pauseSong()
+  } else {
+    playSong()
+  }
+})
