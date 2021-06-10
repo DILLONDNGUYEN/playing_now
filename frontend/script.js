abebaa365e8deb7f9b562ef397ea18c28ae9ad86
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-06-09 21:01:52 -0700

    add const for buttons, and some containers

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,7 @@
 const musicContainer = document.querySelector('.music-container')
+const playbtn = document.querySelector('#play')
+const prevbtn = document.querySelector('#prev')
+const nextbtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
