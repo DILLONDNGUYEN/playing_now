24c44bb4d4e49dafb237fc586cda706b461ee573
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-06-16 13:28:55 -0700

    organization and start the index js section

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -1,1 +1,8 @@
-const
+const playlistContainer = document.getElementById('playlist-container')
+const playlist = document.getElementById('playlist')
+const song = document.getElementById('song')
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+  
+})
