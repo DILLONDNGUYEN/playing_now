5bf71b1ec4b64ef12a85485c3833bef576d585c9
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-06-17 21:07:32 -0700

    add userApi class

---
+++ frontend/javascripts/services/userApi.js
@@ -0,0 +1,13 @@
+class UserApi {
+    static fetchUsers() {
+        fetch("http://localhost:3000/users")
+        .then(resp => resp.json())
+        .then(json => { 
+            json.forEach(user => {
+                User.findOrCreateUser(user)
+                User.playlists
+            });
+    })
+    }
+    
+}
